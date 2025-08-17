2608b028046ad98988f0cd0e1f9b9dcd87e6cdd1
Gaurav1922 <[gauravmanjunath25@gmail.com])>
2025-08-17 14:13:09 +0530

    initial commit

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+from django.conf import settings
+from django.conf.urls.static import static
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9nu1lp8ltjj%%41s0n(z@(lvzk%$%kwl)of#m)u3-ju@z-lb7$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ems-dashboard-jnky.onrender.com","localhost", "127.0.0.1"]
 
 
 # Application definition
