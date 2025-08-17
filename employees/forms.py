9da56456ca102095bf5d319f20ce59940a048dc8
Gaurav1922 <[gauravmanjunath25@gmail.com])>
2025-08-17 14:26:26 +0530

    edit

--- employees/forms.py
+++ employees/forms.py
@@ -1,18 +1,15 @@
 from django import forms
 from .models import Employee, Department
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from django.forms.widgets import PasswordInput, TextInput
 
 
-class createUserForm(UserCreationForm):
+"""class createUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput())
-    password = forms.CharField(widget=PasswordInput())
+    password = forms.CharField(widget=PasswordInput())"""
 
 class EmployeeForm(forms.ModelForm):
     class Meta:
